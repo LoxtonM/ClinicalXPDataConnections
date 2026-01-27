c9b4f5d15914c6590dface8f909ebf8d18ed5360
LOXTON, Martin (BIRMINGHAM WOMEN'S AND CHILDREN'S NHS FOUNDATION TRUST) <martin.loxton@nhs.net>
2026-01-27 15:30:05 +0000

    search tel and email

--- Meta/PatientSearchDataAsync.cs
+++ Meta/PatientSearchDataAsync.cs
@@ -13,6 +13,8 @@ namespace ClinicalXPDataConnections.Meta
         public Task<List<Patient>> GetPatientsListByDOB(DateTime dob);
         public Task<List<Patient>> GetPatientsListByPostCode(string postCode);
         public Task<List<Patient>> GetPatientsListByStaffCode(string staffCode);
+        public Task<List<Patient>> GetPatientsListByTelNo(string telNo);
+        public Task<List<Patient>> GetPatientsListByEmail(string email);
         //the reason for multiple "GetPatientsLists", and not one with multiple parameters, is because in order to do that,
         //the "patients" list would have to be created first and then narrowed by criteria.
         //This would result in very long loading times, as there are a LOT of patients, and I don't really want to select them all
@@ -71,26 +73,25 @@ namespace ClinicalXPDataConnections.Meta
 
         public async Task<List<Patient>> GetPatientsListByStaffCode(string staffCode)
         {
-            /*
-            List<Patient> patients = new List<Patient>();
-            PatientData pData = new PatientData(_clinContext);
-
-            List<Referral> referrals = _clinContext.Referrals.Where(r => r.PATIENT_TYPE_CODE == staffCode || r.GC_CODE == staffCode).ToList();
-
-            foreach (Referral referral in referrals)
-            {
-                Patient patient = pData.GetPatientDetails(referral.MPI);
-                patients.Add(patient);
-            }
-
-            return patients;
-            */
-
             var patients = from patient in _clinContext.Patients
                            join referral in _clinContext.Referrals
                            on patient.MPI equals referral.MPI
                            where referral.PATIENT_TYPE_CODE == staffCode || referral.GC_CODE == staffCode
                            select patient;
+
+            return await patients.ToListAsync();
+        }
+
+        public async Task<List<Patient>> GetPatientsListByTelNo(string telNo)
+        {
+            IQueryable<Patient> patients = _clinContext.Patients.Where(p => p.TEL.Contains(telNo) || p.WORKTEL.Contains(telNo) || p.PtTelMobile.Contains(telNo));
+
+            return await patients.ToListAsync();
+        }
+
+        public async Task<List<Patient>> GetPatientsListByEmail(string email)
+        {
+            IQueryable<Patient> patients = _clinContext.Patients.Where(p => p.EmailAddress.Contains(email));
 
             return await patients.ToListAsync();
         }
